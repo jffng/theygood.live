a2739e08f0dbe66f7bf6bd70d4582430f59a6543
Jeff Ong <jeff.ong@automattic.com>
2019-05-19 15:34:30 -0500

    attempt to fix weird tabbing

--- components/EventDetail.js
+++ components/EventDetail.js
@@ -28,8 +28,8 @@ export default class EventDetail extends React.Component {
 		this.setState({ loading: true });
 
 		fetch( endpoint )
-			.then(res => res.json())
-			.then(res => {
+		.then(res => res.json())
+		.then(res => {
 			this.setState({
 				data: res.event,
 				error: res.error || null,
@@ -52,34 +52,34 @@ export default class EventDetail extends React.Component {
 	render(){
 		const { data } = this.state;
 		const mapOptions = {
-			query: data ? data.venue + ', ' + data.address + ', ' + data.city : null,
-			travelType: 'walk',
-			provider: 'google'
+		query: data ? data.venue + ', ' + data.address + ', ' + data.city : null,
+		travelType: 'walk',
+		provider: 'google'
 		};
 		return (
 			<ThemeProvider>
-				{
-					this.state.loading ? <Text>Loading...</Text> : 
-					<ScrollView style={{ marginLeft: 12, marginRight: 12 }}>
-						<Text style={ styles.label }>Event</Text>
-						<Text style={ styles.name }>{ data.name }</Text>
-						<Divider style={{ backgroundColor: '#CED0CE' }} />
-						<Text style={ styles.label }>Artists</Text>
-						{
-							this.renderArtists()
-						}
-						<Divider style={{ backgroundColor: '#CED0CE' }} />
-						<Text style={ styles.label }>Cover / Tickets</Text>
-						<Button title={ data.isSoldOut ? 'Sold Out' : data.priceAsString } type="clear" containerStyle={{ alignItems: "flex-start" }} buttonStyle={{ padding: 0, marginBottom: 0 }} titleStyle={ styles.venue } onPress={ () => Linking.openURL( data.ticketsURL || data.url ) } />
-						<Text style={ styles.venue }>{  }</Text>
-						<Divider style={{ backgroundColor: '#CED0CE' }} />
-						<Text style={ styles.label }>Venue</Text>
-						<Button title={ data.venue } type="clear" containerStyle={{ alignItems: "flex-start" }} buttonStyle={{ padding: 0, marginBottom: 8 }} titleStyle={ styles.venue } onPress={ () => openMap( mapOptions ) } />
-						<Divider style={{ backgroundColor: '#CED0CE' }} />
-						<Text style={ styles.label }>Description</Text>
-						<Text style={ styles.description }>{ data.description }</Text>
-					</ScrollView>
-				}
+			{
+				this.state.loading ? <Text>Loading...</Text> : 
+				<ScrollView style={{ marginLeft: 12, marginRight: 12 }}>
+					<Text style={ styles.label }>Event</Text>
+					<Text style={ styles.name }>{ data.name }</Text>
+					<Divider style={{ backgroundColor: '#CED0CE' }} />
+					<Text style={ styles.label }>Artists</Text>
+					{
+						this.renderArtists()
+					}
+					<Divider style={{ backgroundColor: '#CED0CE' }} />
+					<Text style={ styles.label }>Cover / Tickets</Text>
+					<Button title={ data.isSoldOut ? 'Sold Out' : data.priceAsString } type="clear" containerStyle={{ alignItems: "flex-start" }} buttonStyle={{ padding: 0, marginBottom: 0 }} titleStyle={ styles.venue } onPress={ () => Linking.openURL( data.ticketsURL || data.url ) } />
+					<Text style={ styles.venue }>{  }</Text>
+					<Divider style={{ backgroundColor: '#CED0CE' }} />
+					<Text style={ styles.label }>Venue</Text>
+					<Button title={ data.venue } type="clear" containerStyle={{ alignItems: "flex-start" }} buttonStyle={{ padding: 0, marginBottom: 8 }} titleStyle={ styles.venue } onPress={ () => openMap( mapOptions ) } />
+					<Divider style={{ backgroundColor: '#CED0CE' }} />
+					<Text style={ styles.label }>Description</Text>
+					<Text style={ styles.description }>{ data.description }</Text>
+				</ScrollView>
+			}
 			</ThemeProvider>
 		);
 	}
@@ -112,5 +112,5 @@ const styles = StyleSheet.create({
 		marginTop: 12,
 		marginBottom: 4,
 		color: '#444'
-	},
+	}
 });
